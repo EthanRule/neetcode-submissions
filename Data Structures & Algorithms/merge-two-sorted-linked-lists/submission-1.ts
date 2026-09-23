e4db81445137636e98d966c6754f44d05419669e
Ethan Rule <94942533+EthanRule@users.noreply.github.com>
2026-09-23 05:32:28 -0700

    Add: merge-two-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-1.ts
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} list1
+     * @param {ListNode} list2
+     * @return {ListNode}
+     */
+    mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode {
+        const dummy = new ListNode();
+        let cur = dummy;
+
+        while (list1 && list2) {
+            if (list1.val <= list2.val) {
+                cur.next = list1;
+                list1 = list1.next;
+            } else {
+                cur.next = list2;
+                list2 = list2.next;
+            }
+
+            cur = cur.next;
+        }
+
+        if (!list1 && list2) {
+            cur.next = list2;
+        }
+
+        if (!list2 && list1) {
+            cur.next = list1;
+        }
+
+        return dummy.next;
+    }
+}
